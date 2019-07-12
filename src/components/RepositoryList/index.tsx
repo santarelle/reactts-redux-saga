721397a6cc85dcdb5daea562fb5ff21dc6fccf32
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2019-07-11 21:42:59 -0400

    Declare Props and State for Component

---
+++ src/components/RepositoryList/index.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+interface Repository {
+  id: number
+  name: string
+}
+
+interface Props {
+  repositories: Repository[]
+}
+
+interface State {
+  newRepository?: string
+}
+
+export default class RepositoryList extends Component<Props, State> {
+  state = {
+    newRepository: '',
+  }
+
+  componentDidMount() {}
+
+  render() {
+    const { repositories } = this.props;
+    const { newRepository } = this.state;
+
+    return (
+      <ul>
+        {repositories.map(repo => <li>{repo.name}</li>)}
+      </ul>
+    );
+  }
+}
